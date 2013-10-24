58c53202ac98695c89856dd5f15ae3c3e26b838d
Zachary Drudi <zachdrudi@gmail.com>
2013-10-24 15:24:38 -0700

    still working on kwok lau

--- cluster.py
+++ cluster.py
@@ -3,6 +3,7 @@ import scipy as sp
 import math
 import scipy.sparse as mlib
 import scipy.sparse.linalg as lalib
+import scipy.sparse.csgraph as csglib
 # clustering algorithms
 # make another file for sparsification algorithms?
 
@@ -18,6 +19,8 @@ import scipy.sparse.linalg as lalib
 def kwok_lau(graph, v, k, epsilon, bigC, littleC):
     num_vertices = graph.shape[0]
     volbd = k * littleC
+    vertices = list(range(num_vertices))
+
     p = [np.zeros(num_vertices, int)]
     p[0][v] = 1
     last = p[-1]
@@ -26,7 +29,9 @@ def kwok_lau(graph, v, k, epsilon, bigC, littleC):
 # need to get W!
     I = mlib.identity(num_vertices, int)
 # assuming symmetric here
-    D = I.multiply(graph.dot(graph))
+    L, D_vector = csglib.laplacian(graph, return_diag=True)
+    D = mlib.diags(D_vector, (0))
+
     lazy_walk = 0.5 * (I + lalib.inv(D) * graph)
 
     num_iterations = math.ceil(num_vertices ** 2 * math.log(num_vertices, 2))
@@ -38,17 +43,26 @@ def kwok_lau(graph, v, k, epsilon, bigC, littleC):
     sortkey = lambda t: (lambda vertex: p[t][vertex] / D[v,v])
 
 # initialize set now
-    # S[0, 0] = []
-    # outset = S[0,0]
-    # outcond = conductance(outset)
-    # for t in range(num_iterations):
+    S = dict()
+
+# don't initialize S  like this. don't init at all?
+    S[0,1] = p[0][v]
+    outset = S[0,1]
+# when S has one element, the conductance is 1
+# conductance is <= 1
+    outcond = 2
+    for t in range(1, num_iterations):
 # so#rt all at once here?
-    #    for j in range(num_vertices):
-    #        # find smallest S_{t,j}
-    #        currcond = conductance(S[t,j])
-    #        if (currcond < outcond and volume(S[t,j]) <= volbd):
-    #            outset = S[t,j]
-    #            outcond = currcond
+        p[t] = p[t-1] * lazy_walk
+    
+        for j in range(1, num_vertices+1):
+# compute new S[t,j]
+            S[t,j] = computeS(sortkey(t), vertices, j, num_vertices)
+            # find smallest S_{t,j}
+            currcond = conductance(S[t,j])
+            if (currcond < outcond and volume(S[t,j]) <= volbd):
+                outset = S[t,j]
+                outcond = currcond
 
 #helper fcns
 # conductance
@@ -56,5 +70,24 @@ def kwok_lau(graph, v, k, epsilon, bigC, littleC):
 # compute S_{t,j} somehow
 # involves sorting
 
-def conductance(csgraph):
-    return None
+# computes conductance of S
+# assume S is in np.array form
+def conductance(chi_s, L, D):
+    return (chi_s * L * chi_s).sum() / volume(chi_s, D)
+
+def volume(chi_s, D):
+    return (chi_s * D * chi_s).sum()
+
+def computeS(keyfcn, vertex_list, size, nvertices):
+    # sort vlist using keyfcn, in descending order
+# get the first size many vertices in list
+    #return the np array version corresponding to that set (chi it)
+    vertex_list.sort(key=keyfcn, reverse=True)
+    return set2vec(vertex_list[:size], nvertices)
+
+def set2vec(S, length):
+# length is the length of desired np array guy
+    chiS = np.zeros(length)
+    for s in S:
+        chiS[s] = 1
+    return chiS
